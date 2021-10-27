e6e7f6e5e8c804710fc8b674ee60038597d043bf
pra24veen13 <praveenpk2413@gmail.com>
2021-10-27 18:16:52 +0530

    today,s task 27-10-2021

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -9,12 +9,32 @@ export class GalleryComponent implements OnInit {
 
   constructor() { }
 
-  rate=14;
+  rate=10.5;
   value1:any="";
-  
+  bg_img="url('../../assets/images/12345.jpg')";
 
-  press(event:any){
+  press(event:any): void{
     this.rate=event.target.value;
+
+    if(this.rate == 10.5)
+  {
+   this.bg_img = "url('../../assets/images/personal-loan_1627244736887.webp')";
+   
+  }
+  if (this.rate == 8)
+  {
+    this.bg_img = "url('../../assets/images/123.jpg')";
+  }
+
+  if (this.rate == 8.5)
+  {
+    this.bg_img = "url('../../assets/images/1234.webp')";
+  }
+
+  if (this.rate == 9)
+  {
+    this.bg_img = "url('../../assets/images/10.jpg')";
+  }
   }
 
 
